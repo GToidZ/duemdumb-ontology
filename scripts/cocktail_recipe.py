b7b33ec54d6dcf3907b6eb6b7e4a765fab42f844
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-11-25 20:30:40 +0700

    feat: add CocktailCheck SWRL rule

--- scripts/cocktail_recipe.py
+++ scripts/cocktail_recipe.py
@@ -11,40 +11,46 @@ outfile = os.path.abspath(os.path.join(directory, "..", "out", f"{name}.html"))
 graph = g.Graph()
 graph.parse(ontology, format='xml')
 
-COCKTAIL_NAME = "BlackRussian"  # Define your cocktail individual
+def cocktail_recipe(cocktail: str) -> str:
+    return """
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX : <http://www.semanticweb.org/lone_lee/ontologies/2023/10/DuemDumb#>
 
-results = graph.query("""
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX : <http://www.semanticweb.org/lone_lee/ontologies/2023/10/DuemDumb#>
+    SELECT ?ing ?amount ?unit
+        WHERE {
+            ?c rdf:type :Cocktail;
+            :hasEntry ?e.
 
-SELECT ?ing ?amount ?unit
-    WHERE {
-		?c rdf:type :Cocktail;
-		:hasEntry ?e.
+            ?e :hasIngredient ?ing;
+                :hasAmount ?amount;
+                :useMeasurement ?unit.
+            
+            FILTER (?c = :%s)
+    }
+    """ % cocktail
 
-		?e :hasIngredient ?ing;
-			:hasAmount ?amount;
-			:useMeasurement ?unit.
-} VALUES ?c { :%s }
-""" % COCKTAIL_NAME)
+if __name__ == "__main__":
+    
+    table = PrettyTable()
+    table.field_names = [
+        "Ingredient",
+        "Amount",
+        "Unit",
+    ]
+    table.align = "l"
+    
+    cocktail = "BlackRussian"  # Define your Cocktail individual
+    query_str = cocktail_recipe(cocktail)
+    
+    for row in graph.query(query_str):
+        table.add_row([row.ing,
+                    round(float(row.amount), 2),
+                    row.unit])
 
-table = PrettyTable()
-table.field_names = [
-    "Ingredient",
-    "Amount",
-    "Unit",
-]
-table.align = "l"
-
-for row in results:
-    table.add_row([row.ing,
-                   round(float(row.amount), 2),
-                   row.unit])
-
-print(table.get_string())
-with open(outfile, "w+") as f:
-    f.write(f"<h1>{COCKTAIL_NAME}</h1><br>" + 
-    table.get_html_string()
-    )
+    print(table.get_string())
+    with open(outfile, "w+") as f:
+        f.write(f"<h1>{cocktail}</h1><br>" + 
+        table.get_html_string()
+        )
