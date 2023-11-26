3b9cfbc2f2e3633dd5cb002564db325b7dedd331
GToidZ <39559733+GToidZ@users.noreply.github.com>
2023-11-26 12:24:32 +0700

    feat: add SPARQL query to recommend drinks based on occassions

---
+++ scripts/drink_recommendation.py
@@ -0,0 +1,77 @@
+import os
+from typing import List
+import rdflib.graph as g
+from pathlib import Path
+from prettytable import PrettyTable
+
+name = Path(__file__).stem
+directory = os.path.dirname(__file__)
+ontology = os.path.abspath(os.path.join(directory, "..", "ontologies", "DuemDumb_V2.1.rdf"))
+outfile = os.path.abspath(os.path.join(directory, "..", "out", f"{name}.html"))
+
+graph = g.Graph()
+graph.parse(ontology, format='xml')
+
+def drink_recommendation():
+    return """
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX : <http://www.semanticweb.org/lone_lee/ontologies/2023/10/DuemDumb#>
+
+SELECT ?o ?c
+WHERE {
+    ?o rdf:type :Occasion;
+        :standardDrinkThreshold ?trsh.
+    
+    # Retrieve cocktail and its total volume along with ABV.
+    { SELECT ?c
+	    (sum(?tvol) AS ?totalVol)
+        ((sum(?avol)/sum(?tvol))*100 as ?abv)
+    WHERE {
+        ?c rdf:type :Cocktail;
+            :hasEntry ?e.
+        
+        ?e :hasIngredient ?ingredient;
+			:hasAmount ?amount;
+			:useMeasurement ?measurement.
+
+        OPTIONAL {
+			?ingredient :hasABV ?abv_temp.
+		}
+		OPTIONAL {
+			?measurement rdf:type :LiquidMeasurement;
+				 :milliliterMultiplier ?mm_temp.
+		}
+		BIND(IF(bound(?mm_temp), ?amount * ?mm_temp, 0) AS ?tvol)
+		BIND(IF(bound(?abv_temp), ?abv_temp / 100, 0) AS ?abv)
+		
+		BIND(IF(?abv > 0, ?abv * ?tvol, 0) AS ?avol)
+    } GROUP BY ?c }
+    
+    BIND((?totalVol * (?abv / 100) * 0.79) / 10 AS ?standardDrink)  # standard drink equivalence
+    
+    FILTER(?standardDrink <= ?trsh)
+}
+    """
+
+if __name__ == "__main__":
+    table = PrettyTable()
+    table.field_names = [
+        "Occasion",
+        "Cocktail",
+    ]
+    table.align = "l"
+    
+    query_str = drink_recommendation()
+    
+    for row in graph.query(query_str):
+        table.add_row([
+            row.o,
+            row.c
+        ])
+    
+    print(table.get_string())
+    
+    with open(outfile, "w+") as f:
+        f.write(table.get_html_string())
