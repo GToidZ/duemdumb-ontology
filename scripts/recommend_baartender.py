6f68bb371300180b70b68cf94cb4b96fb9e3391e
Maroj Thangthong <maroj.t@ku.th>
2023-11-27 16:14:32 +0700

    finishhhhh

--- scripts/recommend_baartender.py
+++ scripts/recommend_baartender.py
@@ -17,31 +17,55 @@ PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX : <http://www.semanticweb.org/lone_lee/ontologies/2023/10/DuemDumb#>
 
-SELECT ?bartender ?inv where{
-    ?cocktail rdf:type :Cocktail;
-        :hasEntry ?e.
-    ?e :hasIngredient ?ing.
+    # We can check if the recipe satisfies with inventory by checking number of existing ingredients w/ number of ingredients in recipe (using COUNT())
 
-    { SELECT ?bartender WHERE{
-        ?bartender rdf:type :Bartender
-            :hasInventory ?inv
-    } 
-    }
-        
-}
+SELECT ?bartender (?asking AS ?cocktail) WHERE {
 	
+	{
+		SELECT ?asking (COUNT(?ingredient) AS ?requiredIngredients) WHERE {
+            ?asking rdf:type :Cocktail;
+			    :hasEntry ?e.
+			?e :hasIngredient ?ingredient.
+		} GROUP BY ?asking
+	}
+
+	{
+		SELECT ?bartender ?targetBartender ?answering ?inv (COUNT(?equal) AS ?hasIngredients) WHERE {
+			?answering rdf:type :Cocktail;
+                :hasEntry ?e.
+            
+            {
+                SELECT ?bartender ?targetBartender ?inv ?ing ?equal WHERE {
+                    ?bartender rdf:type :Bartender.
+                    ?targetBartender rdf:type :Bartender;
+                        :hasInventory ?inv.
+                    ?e :hasIngredient ?ing.
+                    BIND((?bartender = ?targetBartender && ?inv = ?ing) AS ?equal)
+                }
+			}
+                      
+            FILTER(?equal = true)
+		} GROUP BY ?answering ?bartender
+	}
+	
+    FILTER(?asking = ?answering)
+    FILTER(?bartender = ?targetBartender)
+	FILTER(?requiredIngredients = ?hasIngredients)
+}
 """)
 
 table = PrettyTable()
 table.field_names = [
     "bartender",
-    "inv"
+    "cocktail",
 ]
 table.align = "l"
 
 for row in results:
-    table.add_row([row.bartender,
-                    row.inv])
+    table.add_row([
+        row.bartender,
+        row.cocktail,
+    ])
 
 print(table.get_string())
 with open(outfile, "w+") as f:
